245f252a766df8e8b7aa577d9e2d1b898f0498d1
Orestis Koskoletos <orestiskosko@hotmail.com>
2018-11-24 18:42:53 +0200

    MVC setup

---
+++ DeliverNET/DeliverNET/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeliverNET.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            // TODO Add user validation logic
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            // TODO Add singoff
+            return RedirectToAction("Login","Auth");
+        }
+    }
+}
