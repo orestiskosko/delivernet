22fcba2f65e6459d7ddba079118826612422ac3e
Pantazis <stathispantazis7@hotmail.com>
2018-12-18 14:20:58 +0200

    Dashboard

---
+++ DeliverNET/DeliverNET/Controllers/DashboardIndiController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeliverNET.Controllers
+{
+    public class DashboardIndiController : Controller
+    {
+        // Dummy slave dashboard data
+        private static Models.ProfileDummies.IndiDashboardDummyModel Dashboard_Dummy = new Models.ProfileDummies.IndiDashboardDummyModel
+        {
+            totalProfit = 1200,
+            profitMinusTips = 1100,
+            totalOrders = 80,
+            totalHoursWorking = 90,
+            totalDaysWorking = 13,
+            ordersCounterPerDayActive = new List<int> { 2, 5, 3, 10, 4, 3, 6, 5, 2, 15, 14, 6, 5 },
+            tips = 100,
+            ordersCounterPerDayType = new List<int> { 24, 10, 8, 6, 7, 7, 18 }
+        };
+
+
+        public IActionResult DashboardIndexIndi()
+        {
+            return View(Dashboard_Dummy);
+        }
+    }
+}
---
+++ DeliverNET/DeliverNET/Models/ProfileDummies/IndiDashboardDummyModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliverNET.Models.ProfileDummies
+{
+    public class IndiDashboardDummyModel
+    {
+        public float totalProfit { get; set; }
+        public float profitMinusTips { get; set; }
+        public int totalOrders { get; set; }
+        public float totalHoursWorking { get; set; }
+        public float totalDaysWorking { get; set; }
+        public List<int> ordersCounterPerDayActive { get; set; }  // each list item represents a day passed
+        public float tips { get; set; }
+        public List<int> ordersCounterPerDayType { get; set; }  // starting from list[0] as Sunday, each slot in list represents a day
+
+
+    }
+}
